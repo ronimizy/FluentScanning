6d4a4e839f27070c7619f43a5056d1947bf6262f
ronimizy <ronimizy@niuitmo.ru>
2022-02-11 18:25:43 +0300

    chore: .Sample

---
+++ FluentScanning.Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FluentScanning;
+
+var scanner = new AssemblyScanner(typeof(IAssemblyMarker));
+
+var types = scanner.MustBeAssignableTo<A>().ExcludeAbstractClasses().ToArray();
+
+foreach (var t in types)
+{
+    Console.WriteLine(t);
+}
+
+interface IAssemblyMarker { }
+
+abstract class A { }
+
+class B : A { }
+
+class C : A { }
+
+class D { }
