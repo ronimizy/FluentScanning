24eb820bf2a7d00a31bd13aed4a409429d94020c
ronimizy <ronimizy@niuitmo.ru>
2022-02-21 09:23:06 +0300

    docs

--- FluentScanning/Extensions/FilteringExtensions.cs
+++ FluentScanning/Extensions/FilteringExtensions.cs
@@ -7,17 +7,38 @@ namespace FluentScanning
 {
     public static class FilteringExtensions
     {
+        /// <summary>
+        /// Adds a filter to the query defined by user passed function.
+        /// </summary>
         public static IScanningQuery AreSatisfyingCustomFilter(
             this IScanningQuery query, Func<TypeInfo, bool> predicate)
             => query.WithComponent(new TypeFilterComponent(predicate));
 
+        /// <summary>
+        /// Adds a filter to the query defined by ITypeFilterObject.
+        /// </summary>
         public static IScanningQuery AreSatisfyingCustomFilter(this IScanningQuery query, ITypeFilter filter)
             => query.AreSatisfyingCustomFilter(filter.Match);
-        
-        public static IScanningQuery HaveAttribute<TAttribute>(this IScanningQuery query) where TAttribute : Attribute
-            => query.AreSatisfyingCustomFilter(t => t.GetCustomAttribute<TAttribute>() is { });
 
-        public static IScanningQuery DoesNotHaveAttribute<TAttribute>(this IScanningQuery query) where TAttribute : Attribute
-            => query.AreSatisfyingCustomFilter(t => t.GetCustomAttribute<TAttribute>() is { });
+        /// <summary>
+        /// Filters out types that are not marked with specified attribute.
+        /// </summary>
+        public static IScanningQuery HaveAttribute(this IScanningQuery query, Type attributeType)
+            => query.AreSatisfyingCustomFilter(t => t.GetCustomAttribute(attributeType) is { });
+
+        /// <inheritdoc cref="HaveAttribute"/>
+        public static IScanningQuery HaveAttribute<TAttribute>(this IScanningQuery query) where TAttribute : Attribute
+            => query.HaveAttribute(typeof(TAttribute));
+
+        /// <summary>
+        /// Filters out types that are marked with specified attribute.
+        /// </summary>
+        public static IScanningQuery DoesNotHaveAttribute(this IScanningQuery query, Type attributeType)
+            => query.AreSatisfyingCustomFilter(t => t.GetCustomAttribute(attributeType) is { });
+
+        /// <inheritdoc cref="DoesNotHaveAttribute"/>
+        public static IScanningQuery DoesNotHaveAttribute<TAttribute>(this IScanningQuery query)
+            where TAttribute : Attribute
+            => query.DoesNotHaveAttribute(typeof(TAttribute));
     }
 }
